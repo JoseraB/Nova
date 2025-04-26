52311cea14c44b84b78e1b8687b16375d95bb21f
JoseraB <josera.barran08@gmail.com>
2025-04-26 16:17:12 -0400

    finally got home page to work

--- frontend/src/pages/ChatbotHome.jsx
+++ frontend/src/pages/ChatbotHome.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const ChatbotHome = () => {
   return (
     <div>
-      
+       <h1 className="text-3xl font-bold underline">
+      Hello world! </h1>
     </div>
   )
 }
 
-export default Chatbot-Home
+export default ChatbotHome
