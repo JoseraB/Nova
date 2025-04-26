de9f1fbdae260bdf9e165e82b4473100f4579711
JoseraB <josera.barran08@gmail.com>
2025-04-26 16:17:12 -0400

    finally got home page to work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import ChatbotHome from './pages/ChatbotHome'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+ 
+        <Route path ='/chatbothome' element={<ChatbotHome/>}/>
+      </Routes>
     </div>
   )
 }
